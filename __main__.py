829335c18d33c0b533528b52c605820259325bfe
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-07 11:38:37 -0300

    refactor: extract find elements with wait_element
    this commit replaces wait_textbox, get_textbox and while loop for
    send_button methods

--- __main__.py
+++ __main__.py
@@ -7,11 +7,15 @@ from time import sleep
 
 driver = webdriver.Chrome()
 
+def wait_element(by, selector):
+    try: 
+        return driver.find_element(by, selector)
+    except NoSuchElementException:
+        sleep(.2)
+        return wait_element(by, selector)
+
 def get_chat_url(phone):
     return 'https://web.whatsapp.com/send?phone={}&text&type=phone_number&app_absent=0'.format(phone)
-
-def get_textbox():
-    return driver.find_element(By.CSS_SELECTOR, '[data-testid="conversation-panel-wrapper"] [role="textbox"][contenteditable]')
 
 def open_clip():
     clip = driver.find_element(By.CSS_SELECTOR, '[data-testid="conversation-clip"] [role="button"]')
@@ -23,20 +27,6 @@ def open_clip():
 def access_chat(phone):
     return driver.get(get_chat_url(phone))
 
-def wait_textbox():
-    try:
-        textbox = get_textbox()
-
-        print('Chat encontrado!')
-        return textbox
-    except NoSuchElementException:
-        print('Erro ao tentar acessar a caixa de texto')
-        print('tentando novamente em alguns segundos...\n')
-
-        sleep(3)
-
-        return wait_textbox()
-
 def send_message(message, textbox):
     textbox.send_keys(message + Keys.ENTER)
 
@@ -47,19 +37,14 @@ def send_image(image):
     attach_media_button = driver.find_element(By.CSS_SELECTOR, '[data-testid="mi-attach-media"] input[type="file"]')
     attach_media_button.send_keys(image_abs)
 
-    send_button = None
-    while not send_button:
-        try: 
-            send_button = driver.find_element(By.CSS_SELECTOR, '.copyable-area [role="button"] [data-testid="send"]')
-        except NoSuchElementException:
-            sleep(.1)
+    send_button = wait_element(By.CSS_SELECTOR, '.copyable-area [role="button"] [data-testid="send"]')
 
     send_button.click()
 
 def deliver_to_phone(phone):
     access_chat(phone)
 
-    textbox = wait_textbox()
+    textbox = wait_element(By.CSS_SELECTOR, '[data-testid="conversation-panel-wrapper"] [role="textbox"][contenteditable]')
 
     send_message('teste drive', textbox)
     send_image('./message/example.jpg')
