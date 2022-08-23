401a6f3581d1ee70e8880d587c9e586a100e0657
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-23 00:33:59 -0300

    feat: send texts and images in alphabetical order from messages folder

--- __main__.py
+++ __main__.py
@@ -1,3 +1,5 @@
+import os
+from time import sleep
 from utils.access_chat import access_chat
 from utils.send_message import send_message
 from utils.attachments import send_image
@@ -5,8 +7,16 @@ from utils.attachments import send_image
 def deliver_to_phone(phone):
     access_chat(phone)
 
-    send_message('teste drive')
-    send_image('./message/example.jpg')
+    directory = 'messages'
+    for filename in sorted(os.listdir(directory)):
+        file = os.path.join(directory, filename)
+
+        if '.txt' in filename:
+            with open(file) as f:
+                send_message(f.read())
+        else:
+            send_image(file)
+            sleep(2)
 
 if __name__ == '__main__':
     deliver_to_phone('5513997406352')
