5be1318149a9d425f99259536c22c935bd8a56cd
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-23 00:48:09 -0300

    refactor: extract send_from_directory method

--- __main__.py
+++ __main__.py
@@ -1,22 +1,10 @@
-import os
-from time import sleep
 from utils.access_chat import access_chat
-from utils.send_message import send_message
-from utils.attachments import send_image
+from utils.send_from_directory import send_from_directory
 
 def deliver_to_phone(phone):
     access_chat(phone)
 
-    directory = 'messages'
-    for filename in sorted(os.listdir(directory)):
-        file = os.path.join(directory, filename)
-
-        if '.txt' in filename:
-            with open(file) as f:
-                send_message(f.read())
-        else:
-            send_image(file)
-            sleep(2)
+    send_from_directory('messages')
 
 if __name__ == '__main__':
     deliver_to_phone('5513997406352')
---
+++ utils/send_from_directory.py
@@ -0,0 +1,14 @@
+import os
+from utils.send_message import send_message
+from utils.attachments import send_image
+
+def send_from_directory(directory):
+    for filename in sorted(os.listdir(directory)):
+        file = os.path.join(directory, filename)
+
+        if '.txt' in filename:
+            with open(file) as f:
+                send_message(f.read())
+        else:
+            send_image(file)
+            sleep(2)
