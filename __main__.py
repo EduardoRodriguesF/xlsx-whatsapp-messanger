84923a02500cc233431e7745923a3e57cb862eba
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-27 15:02:36 -0300

    feat: loop through numbers accessing chat

--- __main__.py
+++ __main__.py
@@ -1,4 +1,10 @@
-from utils.deliver import deliver_to_list
+from utils.xlsx import for_each_valid_phone
+from utils.access_chat import access_chat
+from time import sleep
 
 if __name__ == '__main__':
-    deliver_to_list(['5513997406352', '5511916717633'])
+    def callback(phone):
+        access_chat(phone)
+        sleep(10)
+
+    for_each_valid_phone(callback)
