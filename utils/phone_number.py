0c12a5f0ab95dff7cc80ae79a7fff2f258b9a45c
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-27 13:28:39 -0300

    feat(sanitize_phone): add 55 country code

--- utils/phone_number.py
+++ utils/phone_number.py
@@ -4,6 +4,11 @@ def is_phone_valid(phone):
     return len(phone) > 13
 
 def sanitize_phone(phone):
+    if (len(phone) < 3): return ''
+
     result = re.sub('[^0-9]', '', phone)
+
+    if (result[0] != '5' or result[1] != '5'):
+        result = '55' + result
 
     return result
