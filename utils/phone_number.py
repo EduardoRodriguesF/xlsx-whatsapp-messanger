94bdeb3a7399d6d968080b40d8955dc2a7c939ef
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-28 14:48:45 -0300

    fix(phone_number): prevent crashing from nullish values

--- utils/phone_number.py
+++ utils/phone_number.py
@@ -1,6 +1,8 @@
 import re
 
 def is_phone_valid(phone):
+    if not phone: return False
+
     result = phone.split('/')[0].strip()
 
     return len(result) == 13 or len(result) == 12
@@ -9,6 +11,8 @@ def sanitize_phone(phone):
     if (len(phone) < 3): return ''
 
     result = re.sub('[^0-9]', '', phone)
+
+    if not result: return ''
 
     if (result[0] != '5'):
         result = '55' + result
