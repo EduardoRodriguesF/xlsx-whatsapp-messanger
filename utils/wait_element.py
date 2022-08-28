012b4103e98e10862ffb1d55f98afe369120bf73
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-28 15:13:14 -0300

    feat(wait_element): fallback popup element to prevent blocking

--- utils/wait_element.py
+++ utils/wait_element.py
@@ -1,10 +1,15 @@
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 from driver import driver
 
 def wait_element(by, selector):
     try: 
-        return driver.find_element(by, selector)
+        try:
+            fallback_element = driver.find_element(By.CSS_SELECTOR, '[role="button"][data-testid="popup-controls-ok"]')
+            return False
+        except NoSuchElementException:
+            return driver.find_element(by, selector)
     except NoSuchElementException:
         sleep(.2)
         return wait_element(by, selector)
