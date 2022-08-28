91a65bd9c373b74c7a4ceca8040690f1b68ab310
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-28 15:16:27 -0300

    feat(access_chat): return if chat is available or not

--- utils/access_chat.py
+++ utils/access_chat.py
@@ -1,5 +1,12 @@
 from driver import driver
+from utils.wait_element import wait_element
+from selenium.webdriver.common.by import By
 
 def access_chat(phone):
     url = 'https://web.whatsapp.com/send?phone={}&text&type=phone_number&app_absent=0'.format(phone)
-    return driver.get(url)
+
+    driver.get(url)
+
+    chat_confirmation = wait_element(By.CSS_SELECTOR, '[data-testid="conversation-panel-wrapper"] [role="textbox"][contenteditable]')
+
+    return bool(chat_confirmation)
