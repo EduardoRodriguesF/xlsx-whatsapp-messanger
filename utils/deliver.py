5c309a5abd1de35214e06abc7791be66fc72fadc
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-29 23:45:38 -0300

    feat(deliver_to_phone): return whether succeeded or not sending messages

--- utils/deliver.py
+++ utils/deliver.py
@@ -5,9 +5,12 @@ from utils.send_from_directory import send_from_directory
 def deliver_to_phone(phone):
     is_chat_available = access_chat(phone)
 
-    if not is_chat_available: return print('Unavailable chat:', phone)
+    if not is_chat_available:
+        print('Unavailable chat:', phone)
+        return False
 
     send_from_directory('messages')
+    return True
 
 def deliver_to_list(phone_list):
     for phone in phone_list:
